6ab55e76f60c4c9af3d5f53da38a18d0f0dbccc1
FrontierV <ultrachronop@gmail.com>
2016-08-24 11:17:35 +0300

    added basic components

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
-alert();
+var Authorize = React.createClass({
+  render: function() {
+    return (
+      <div className="authorize">
+        <button>vk.com авторизация</button>  
+      </div>
+    );
+  }
+});
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className="app">
+        <Authorize/>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
